89d84590aa32b9f10c45ad3d5ce4d8babe76463d
matheusluepb <matheus.lucena@aluno.uepb.edu.br>
2025-12-08 17:13:54 -0300

    Add files via upload
    ver 0.2
    - Login
    - Informações
    - Home

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,11 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
     repositories {
         google()
         mavenCentral()
